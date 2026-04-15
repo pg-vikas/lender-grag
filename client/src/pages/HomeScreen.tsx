5a6447ba3b14470d97398c97449538219240704b
chayan6 <51744929-chayan6@users.noreply.replit.com>
2026-04-15 01:13:40 +0000

    Restored to '554c932729d5a86c28472f2b324786acae4351bc'
    Replit-Restored-To: 554c932729d5a86c28472f2b324786acae4351bc

--- client/src/pages/HomeScreen.tsx
+++ client/src/pages/HomeScreen.tsx
@@ -8,7 +8,6 @@ import { BrokerAdvantageSection } from "./sections/BrokerAdvantageSection";
 import { ProcessSection } from "./sections/ProcessSection";
 import { ReviewsSection } from "./sections/ReviewsSection";
 import { ResourcesSection } from "./sections/ResourcesSection";
-import { LearnBeforeYouBorrowSection } from "./sections/LearnBeforeYouBorrowSection";
 import { AccountPortalSection } from "./sections/AccountPortalSection";
 import { FAQSection } from "./sections/FAQSection";
 import { FinalCTASection } from "./sections/FinalCTASection";
@@ -20,7 +19,6 @@ export const HomeScreen = (): JSX.Element => {
       <Header />
       <HeroSection />
       <ResourcesSection />
-      <LearnBeforeYouBorrowSection />
       <StatsSection />
       <CalculatorSection />
       <BrokerAdvantageSection />
